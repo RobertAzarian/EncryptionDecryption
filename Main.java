a7cf34fa598676b4890935e53359087018a6bb89
RobertAzarian <az.roberto1998@gmail.com>
2023-05-13 21:12:03 +0400

    Stage 5/6: X-files

--- Main.java
+++ Main.java
@@ -1,26 +1,47 @@
 package encryptdecrypt;
 
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) throws NumberFormatException {
         String operation = "enc";
         int key = 0;
         String message = "";
+        File fileOutput = null;
+        File fileInput = null;
 
         for (int i = 0; i < args.length; i++) {
             switch (args[i]) {
                 case "-mode" -> operation = "dec".equals(args[i + 1]) ? "dec" : "enc";
                 case "-key" -> key = Integer.parseInt(args[i + 1]);
                 case "-data" -> message = args[i + 1];
+                case "-in" -> fileInput = new File(args[i + 1]);
+                case "-out" -> fileOutput = new File(args[i + 1]);
             }
         }
+        if (fileInput != null) {
+            try (BufferedReader br = new BufferedReader(new FileReader(fileInput))) {
+                message = br.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        String result = operation(operation, message, key);
 
-        System.out.println(operation(operation, message, key));
+        if (fileOutput != null) {
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(fileOutput))) {
+                bw.write(result);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            System.out.println(result);
+        }
     }
 
     public static String operation(String operation, String message, int key) {
         char[] chars = message.toCharArray();
         StringBuilder sb = new StringBuilder();
-
 
         switch (operation) {
             case "enc" -> {
